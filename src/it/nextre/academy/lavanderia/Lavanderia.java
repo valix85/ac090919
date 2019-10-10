2741a8dc2aff9b1355bbbb7ad3990f91a0a3c638
valix85@gmail.com <valix85@gmail.com>
2019-10-10 10:17:41 +0200

    Stream e lambda in lavanderia

--- src/it/nextre/academy/lavanderia/Lavanderia.java
+++ src/it/nextre/academy/lavanderia/Lavanderia.java
@@ -4,11 +4,15 @@ import it.nextre.academy.myutils.DummyData;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Random;
 
 public class Lavanderia {
 
     public void lava(List<Capo> capi){
+        Random r = new Random();
         for (Capo tmp : capi){
+            //randomicamente alcuni capi non vengono puliti bene e andranno rilavati
+            if (r.nextDouble()<0.25)continue;
             if (!tmp.isPulito()){
                 tmp.setPulito(true);
                 tmp.setPrezzo(DummyData.getRandomDouble(4,3));
--- src/it/nextre/academy/lavanderia/LavanderiaMain.java
+++ src/it/nextre/academy/lavanderia/LavanderiaMain.java
@@ -2,6 +2,12 @@ package it.nextre.academy.lavanderia;
 
 import it.nextre.academy.myutils.DummyData;
 
+import java.util.List;
+import java.util.Set;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class LavanderiaMain {
 
     /*
@@ -31,9 +37,51 @@ public class LavanderiaMain {
 
         //stampare in ordine di prezzo i capi lavati
         c1.getCapiLavatiPerPrezzo().forEach(System.out::println);
+        double totale = c1.getCapiLavatiPerPrezzo()
+                .stream()
+                .mapToDouble(capo->capo.getPrezzo())
+                .sum();
+                //.reduce(0, (left, right) -> left+right);
+                //Conteggio
+                //.count();
+                //.reduce(0, (a,b)->a+1); //a elemento iniziale, b il prezzo, il risultato viene messo in a
+        System.out.println(totale);
 
+
+
+        //estrazione delle categorie dei capi lavati non doppie!
+        List<String> catsPulite = c1.getCapiLavatiPerPrezzo()
+                .stream()
+                .map(capo -> capo.getCategoria().name().toLowerCase())
+                /*
+                .map(new Function<Capo, String>() {
+                    @Override
+                    public String apply(Capo capo) {
+                        return capo.getCategoria().name().toLowerCase();
+                    }
+                })
+                */
+                //.peek(System.out::println)
+                //.filter(cat->cat.length()>7)
+                .distinct()//inutile se colleziono in un set
+                .collect(Collectors.toList());
+
+        catsPulite.forEach(MyPrinter::stampaS);
+        catsPulite.forEach(el->MyPrinter.stampaS(el));
+
+        MyPrinter myPrinter=new MyPrinter();
+        catsPulite.forEach(myPrinter::stampaI);
 
     }//end main
 
 
 }//end class
+
+class MyPrinter{
+    public static void stampaS(Object o){
+        System.out.println("item Statico: " + o.toString());
+    }
+    public void stampaI(Object o){
+        System.out.println("item Istanza: " + o.toString());
+    }
+}
